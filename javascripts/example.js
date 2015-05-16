a3391a18911387e47ab31bc4b326a43a2ff117bc
54017 <571738739@qq.com>
2015-05-16 14:40:12 -0300

    done

--- javascripts/example.js
+++ javascripts/example.js
@@ -1,5 +1,5 @@
         alert("1");
         alert("3");
         $("a[class^='year_2015'").css("color", "red");
-        $("a[class^='month_2015_05'").css("color", "rgba(17,209,255, 0.8)");
+        $(".item").css("backgroundColor", "rgba(17,209,255, 0.8)");
         alert("2");
