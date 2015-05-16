5d371059268d413d6208607a07789798157d4ff2
54017 <571738739@qq.com>
2015-05-16 14:10:24 -0300

    done

--- javascripts/example.js
+++ javascripts/example.js
@@ -1,5 +1,2 @@
-$(function() {
-        $("a[class^='year_2015'").css("color", "rgba(17,209,255, 0.8)");
-        $("a[class^='month_2015_05'").css("color", "rgba(17,209,255, 0.8)");
-        alert("1");
-});
+alert("1");
+$(".item").css("backgroundColor", "black");
