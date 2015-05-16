073fea558ed154f095a8e5bd604180b4a6c7c6ce
54017 <571738739@qq.com>
2015-05-16 14:41:45 -0300

    done

--- javascripts/example.js
+++ javascripts/example.js
@@ -1,5 +1,7 @@
-        alert("1");
-        alert("3");
-        $("a[class^='year_2015'").css("color", "red");
-        $(".item").css("backgroundColor", "rgba(17,209,255, 0.8)");
-        alert("2");
+$(function() {
+    alert("1");
+    alert("3");
+    $(".item").css("backgroundColor", "rgba(17,209,255, 0.8)");
+    alert("2");
+})
+        
